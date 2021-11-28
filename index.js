c1d281d7d94b825f2cb49291bb3586116ef1f26f
Dimitar Vashkov <dimitarvashkov@outlook.com>
2021-11-28 13:19:44 +0200

    Make async call after timeout

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 // API key f433ba1a
 
-const fetchData = async () => {
+const fetchData = async (searchTerm) => {
 	const response = await axios.get('http://www.omdbapi.com/', {
 		params: {
 			apikey: 'f433ba1a',
-			i: 'tt0848228',
+			s: searchTerm,
 		},
 	});
-	console.log(response.data);
+	if (response.data.Error) {
+		return [];
+	}
+	return response.data.Search;
 };
 
-fetchData();
+const input = document.querySelector('input');
+
+const onInput = async (event) => {
+	const movies = await fetchData(event.target.value);
+	for (let movie of movies) {
+		const div = document.createElement('div');
+
+		div.innerHTML = `
+         <img src="${movie.Poster}" />
+         <h1>${movie.Title}</h1>
+        `;
+		document.querySelector('#target').appendChild(div);
+	}
+};
+input.addEventListener('input', debounce(onInput, 500));
